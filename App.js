77900baf356e26bcf964aeaae7adba02fe9a3a62
ailishnagpal <71159297+ailishnagpal@users.noreply.github.com>
2021-01-15 18:01:07 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Button, View, Text } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+        <View style={{marginTop:250}}>
+        <Button title="click me" color='#907635'> </Button>
+        </View>
+    );
+  }
+}
